6e9d78cedaa7d51e04345f03d8f9a62aff1eacc3
SantiagoSchons <santiagodeoliveira2004@gmail.com>
2025-07-08 18:28:11 -0300

    firts

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("contato-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso! Em breve entraremos em contato.");
+});
